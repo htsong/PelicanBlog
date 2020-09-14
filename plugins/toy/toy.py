9027b68300511f3d01baade35f19279cd6ff5b9f
htsong <htsong@scut.edu.cn>
2020-09-15 06:33:25 +0800

    Create toy.py

---
+++ plugins/toy/toy.py
@@ -0,0 +1,17 @@
+#
+# 《The missing Pelican plugins guide》
+#  Published January 11, 2020 • 10 minutes read
+#  https://blog.geographer.fr/pelican-plugins
+#
+
+from pelican import signals
+
+# Hook function, with the right parameters
+def run(path, context):
+    print(path)
+
+
+# Module entry point
+def register():
+    # Connect the run hook function to the content_written signal
+    signals.content_written.connect(run)
